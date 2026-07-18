1a46e955a69513f4ea48dedb5e81d8f1b1635fe0
harshpunchayil <harshpunchayil@gmail.com>
2026-07-18 08:39:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, sessio
+from flask import Flask, render_template, request, redirect, url_for, session
 import json
 import os
 import threading
